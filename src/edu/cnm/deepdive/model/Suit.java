f05c6c4bcead1d7053947906305819ee3e0d3fa9
Jonathan Cade <jcade@cnm.edu>
2020-01-23 16:55:48 -0700

    Final trick.

--- src/edu/cnm/deepdive/model/Suit.java
+++ src/edu/cnm/deepdive/model/Suit.java
@@ -3,41 +3,20 @@ package edu.cnm.deepdive.model;
 public enum Suit {
 
   CLUBS, DIAMONDS, HEARTS, SPADES;
-
   private static final String[] symbols = {"\u2663", "\u2662", "\u2661", "\u2660"};
-  private static final Color[] colors = {}
-  public String symbol() {Color.BLACK, Color.RED, Color.RED, Color.BLACK};
+  private static final Color[] colors = {Color.BLACK, Color.RED, Color.RED, Color.BLACK};
 
-
-    return symbols[ordinal()];
-
-  }
-// Rank.java
-  private static final String[] symbols = {
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-  };
   public String symbol() {
-    return symbols[ordinal()];
+    return symbols[this.ordinal()];
 
   }
 
-  public Color color() {
+  public Color color(){
     return colors[ordinal()];
   }
+
   public enum Color {
     RED, BLACK;
   }
-
 }
+
