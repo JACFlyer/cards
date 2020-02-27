9c56cbd0f12a26233788fe168019b8e6b1e1b308
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-02-27 10:23:41 -0700

    Deck implements comparator.

--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Random;
 
 /** Creates an array of 52 cards, and initiates random shuffle.*/
-public class Deck {
+public class Deck implements Comparator<Card> {
 
   private List<Card> cards;
   private List<Card> dealt;
@@ -65,7 +65,11 @@ public int dealt() {
     if (gather) {
       gather();
     }
-    cards.sort(Comparator.comparing(Card::getSuit).thenComparing(Card::getRank));
+    cards.sort(this);
   }
 
+  @Override
+  public int compare(Card card1, Card card2) {
+    return Comparator.comparing(Card::getSuit).thenComparing(Card::getRank).compare(card1, card2);
+  }
 }
